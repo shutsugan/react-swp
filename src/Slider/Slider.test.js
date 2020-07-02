6dc129eda20f0dfb32c374af2b778f8faf7a8f6a
Mohamed Tajjiou <mtajjiou@sqli.com>
2020-07-02 19:09:20 +0100

    test(slider): check if the component renders correctly

---
+++ src/Slider/Slider.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Slider from "./Slider";
+
+global.IntersectionObserver = jest.fn(function () {
+  this.observe = jest.fn();
+  this.unobserve = jest.fn();
+  this.disconnect = jest.fn();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <Slider>
+      <div>1</div>
+      <div>2</div>
+    </Slider>
+  );
+  const element = getByText(/1/i);
+  expect(element).toBeInTheDocument();
+});
